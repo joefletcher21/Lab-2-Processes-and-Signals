4faf7ddb76f8a33f02d2974ad778d30ee1f03623
Joseph Fletcher <jckmfletcher@gmail.com>
2020-12-12 12:29:37 -0500

    Update signal.c

--- lab-2--signals/signal.c
+++ lab-2--signals/signal.c
@@ -1,68 +1,23 @@
-/* hello_signal.c */
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
-#include <time.h> 
-#include <sys/wait.h>
 
-
-#define   MAX_COUNT  2
-
-pid_t getpid(void);
-pid_t getppid(void);
-
-void childProcess(){
-
-  // this is what each child process does
-
-  srand(time(0));
-  int numLoop = (rand() % 30 + 1);
-
-  for (int k = 1; k <= numLoop; k++) {
-    int timeSleep = (rand() % 10 + 1);
-
-    // get the pid of the parent and the child
-    pid_t child = getpid();
-    pid_t parent = getppid();
-
-    printf("Child Pid: %d is going to sleep!\n", child);
-
-    // pause execution 
-    sleep(timeSleep);
-
-    printf(" Child Pid: %d is awake!\n Where is my Parent: %d?\n", child, parent);
-  }
-  exit(0);
-
+void handler(int signum)
+{ //signal handler
+  printf("Hello World!\n");
+  //exit(1); //exit after printing
 }
 
-
-int main()
+int main(int argc, char * argv[])
 {
-  int status;
-  int pid[MAX_COUNT];
-
-  // the make amount of child processes should be 2
-  for(int j = 0; j < MAX_COUNT; j++){
-    pid[j] = fork();
-
-    // if a child process is created success
-    if(pid[j] == 0){
-      childProcess();
-    } else if (pid[j] == -1){
-      printf("There was an error in created a child process.");
-    }
-  }
-
-  for(int n = 0; n < MAX_COUNT; n++){
-    //iter twice because 2 children
-    
-    wait(&status); // parent process waits for everything to finish
-
-    // notify
-    printf("Child Pid: %d has completed!\n", pid[n]);
-  }
-
-  return 0;
+  signal(SIGALRM,handler); //register handler to handle SIGALRM
+  alarm(1); //Schedule a SIGALRM for 1 second
+  while(1){
+    signal(SIGALRM,handler);
+    alarm(1);
+    sleep(1);
+    printf("Turing was right!\n");
+  } //busy wait for signal to be delivered
+  return 0; //never reached
 }
